ef0ad70f4add61a042f728e36773c4c65d62db01
Henry Escobar <henrye@gmail.com>
2022-10-27 15:33:53 -0700

    commit tree 26fe6e8b012a298b8d8bc68f3b24057ca9a790cc

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def positive_integers?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def validate_triangle
+    raise TriangleError unless positive_integers? && valid_triangle_inequality?
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "Triangle is invalid"
+    end
+  end
+
 end
